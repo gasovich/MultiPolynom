f68134103fc9f5bba51ae98eab2ae397d9b5118a
Андрей Гасович <gasovich@yandex.ru>
2020-04-05 13:52:45 +0300

    Добавил удаление односвязного списка значений элементов матрицы Попрвил в readme.txt замечание о разделителях в файле .csv

--- matread.cpp
+++ matread.cpp
@@ -6,62 +6,62 @@
 
 
 int main(int argc, char* argv[])
-{
+{// Чтение матрицы из файла .csv с разделителем "пробел" или "табуляция"
 	using namespace std;
 	
 	struct buffer
-	{
+	{// Односвязный список, куда будет изначально прочитана вся матрица
 		double x;
 		buffer* next;
-	} *a, *begin;
+	} *a, *begin, *tmp;
 
-	int n_col = 0;
-	int n_row = 0;
+	int n_col = 0; // Количество столбцов 
+	int n_row = 0; // Количество строк
 	int i = 0, j = 0;
 	string s;
 	double **matrix;
 	
 	ifstream fin(argv[1]); // Открыли файл для чтения
 	
-	a = new buffer;
-	begin = a;
+	a = new buffer; // Создаем начало списка
+	begin = a; // Запоминаем ссылку на первый элемент списка
 	
 	while (true)
-	{
-		getline(fin, s);
-		if (fin.eof()) break;
-		istringstream st(s);
+	{ // Цикл по строкам файла
+		getline(fin, s); // Считываем очередную строку
+		if (fin.eof()) break; // Выход из цикла по достижении конца файла
+		istringstream st(s); // Открываем строковый поток ввода из прочитанной строки
 		while (true) 
-		{
-			st >> a->x;
-			if (!st) break;
-			a->next = new buffer;
-			a = a->next;
-			j++;
-			if (i == 0)
-				n_col = j;
+		{ // Цикл по строке
+			st >> a->x; // Читаем очередное занчение элемента матрицы
+			if (!st) break; // Выход из цикла по достижении конца строки
+			a->next = new buffer; // Создаем новый элемен списка
+			a = a->next; // Переводим указатель на вновь созданный элемент
+			j++; // Увеличиваем счетчик прочитанных из строки значений
+			if (j > n_col) // Если счетчик не достиг числа столбцов, то
+				n_col = j; // обновляем счетчик числа столбцов
 		};
-		j = 0;
-		i++;
-		if (i > n_row)
-			n_row = i;
+		j = 0; // Сбрасываем счетчик столбцов
+		i++; // Увеличиваем счетчик строк
+		if (i > n_row) // Если счетчик строк не достиг максимума, то 
+			n_row = i; // обновляем счетчик числа строк
 	};
-	fin.close();
+	fin.close(); // Закрываем файловый поток ввода
 	
-	matrix = new double*[n_row];
-	for (i = 0; i < n_row; i++)
+	matrix = new double*[n_row]; // Создаем двумерный массив для хранения матрицы
+	for (i = 0; i < n_row; i++) // Создаем строки матрицы
 		matrix[i] = new double[n_col];
 		
-	a = begin;
+	a = begin; // Возвращаемся в начало списка
 	for (i = 0; i < n_row; i++)
-	{
+	{ // Копируем списко в созданный ранее двумерный массив
 		for (j = 0; j < n_col; j++)
 		{
 			matrix[i][j] = a->x;
 			a = a->next;
 		};
 	};
-		
+	// Выводим матрицу на экран
 	cout << "<--- Матрица --->" << endl;
 	for (i = 0; i < n_row; i++)
 	{
@@ -70,7 +70,17 @@ int main(int argc, char* argv[])
 		cout << endl;
 	};
 	
-	for (i = 0; i < n_row; i++)
+	cout << endl << "Число элементов " << n_col * n_row << endl;
+	
+	a = begin; // Возвращаемся в начало списка
+	for (i = 0; i < n_col * n_row; i++)
+	{ // Удаляем весь список значений элементов матрицы
+		tmp = a;
+		a = a->next;
+		delete tmp;
+	};
+	
+	for (i = 0; i < n_row; i++) // Удаляем массив, где хранится матрица
 		delete []matrix[i];
 	delete []matrix;
 }
